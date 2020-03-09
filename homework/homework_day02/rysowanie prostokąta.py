1f21fabd1e7a01123eef6258520daea9ad4f42b4
MartaDobrocinska <m.dobrocinska@gmail.com>
2020-03-09 20:08:44 +0100

    initial commit

--- homework/homework_day02/rysowanie prostokąta.py
+++ homework/homework_day02/rysowanie prostokąta.py
@@ -3,6 +3,32 @@ wysokosc = int(pytanie1)
 pytanie2 = input("Podaj szerokość prostokąta: ")
 szerokosc = int(pytanie2)
 print("+",("-"*szerokosc),"+")
-for user in range(0, szerokosc):
-    print("|"," "*wysokosc,"|")
+
+# for user in range(0, wysokosc):
+#     print("|"," "*szerokosc,"|")
+# print("+",("-"*szerokosc),"+")
+
+if wysokosc > 0:
+    print("|"," "*szerokosc,"|")
+if (wysokosc  - 1) > 0:
+    print("|"," "*szerokosc,"|")
+if (wysokosc - 2) > 0:
+    print("|"," "*szerokosc,"|")
+if (wysokosc - 3) > 0:
+    print("|", " " * szerokosc, "|")
+if (wysokosc - 4) > 0:
+    print("|", " " * szerokosc, "|")
+if (wysokosc - 5) > 0:
+    print("|", " " * szerokosc, "|")
+if (wysokosc - 6) > 0:
+    print("|", " " * szerokosc, "|")
+if (wysokosc - 7) > 0:
+    print("|", " " * szerokosc, "|")
+if (wysokosc - 8) > 0:
+    print("|", " " * szerokosc, "|")
+if (wysokosc - 9) > 0:
+    print("|", " " * szerokosc, "|")
+if (wysokosc - 10) > 0:
+    print("|", " " * szerokosc, "|")
 print("+",("-"*szerokosc),"+")
+
