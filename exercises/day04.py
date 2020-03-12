d466543a9766465513a2be2cfce799beb6cf78e5
MartaDobrocinska <m.dobrocinska@gmail.com>
2020-03-12 18:25:02 +0100

    initial commit

---
+++ exercises/day04.py
@@ -0,0 +1,6 @@
+## GIT ##
+# git add -> <plik> lub <katalog>: plik dodany i podlega kontroli wersji
+# commit -a -> wszystkie zmiany           (zapisuje zmiany)
+#        -m  -> plus "komentarz"
+
+
