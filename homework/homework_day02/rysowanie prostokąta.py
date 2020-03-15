bac406278f2da25a120615bb787f4b5d24cdee9f
MartaDobrocinska <m.dobrocinska@gmail.com>
2020-03-15 12:10:44 +0100

    initial commit

--- homework/homework_day02/rysowanie prostokąta.py
+++ homework/homework_day02/rysowanie prostokąta.py
@@ -2,33 +2,11 @@ pytanie1 = input("Podaj wysokość prostokąta: ")
 wysokosc = int(pytanie1)
 pytanie2 = input("Podaj szerokość prostokąta: ")
 szerokosc = int(pytanie2)
-print("+",("-"*szerokosc),"+")
+srodek = "|" + (" " * szerokosc) + "|\n"
+print("+" + ("-" * szerokosc) + "+")
+print(wysokosc * srodek, end = "")
+print("+" + ("-"*szerokosc) + "+")
 
 # for user in range(0, wysokosc):
 #     print("|"," "*szerokosc,"|")
 # print("+",("-"*szerokosc),"+")
-
-if wysokosc > 0:
-    print("|"," "*szerokosc,"|")
-if (wysokosc  - 1) > 0:
-    print("|"," "*szerokosc,"|")
-if (wysokosc - 2) > 0:
-    print("|"," "*szerokosc,"|")
-if (wysokosc - 3) > 0:
-    print("|", " " * szerokosc, "|")
-if (wysokosc - 4) > 0:
-    print("|", " " * szerokosc, "|")
-if (wysokosc - 5) > 0:
-    print("|", " " * szerokosc, "|")
-if (wysokosc - 6) > 0:
-    print("|", " " * szerokosc, "|")
-if (wysokosc - 7) > 0:
-    print("|", " " * szerokosc, "|")
-if (wysokosc - 8) > 0:
-    print("|", " " * szerokosc, "|")
-if (wysokosc - 9) > 0:
-    print("|", " " * szerokosc, "|")
-if (wysokosc - 10) > 0:
-    print("|", " " * szerokosc, "|")
-print("+",("-"*szerokosc),"+")
-
