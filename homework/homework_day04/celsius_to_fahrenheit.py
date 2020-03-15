e72f64bb9fdb5d03a364d9f5ad17edf06aa4a958
MartaDobrocinska <m.dobrocinska@gmail.com>
2020-03-15 15:53:54 +0100

    initial commit

---
+++ homework/homework_day04/celsius_to_fahrenheit.py
@@ -0,0 +1,20 @@
+def cels_to_fahr(celsius):
+    """converts celsius degrees to fahrenheit"""
+    print(celsius * 9 / 5 + 32)
+
+
+while True:
+    celsius = input("Podaj temperaturę w stopniach Celsjusza: ")
+    try:
+        int(celsius)
+        break
+    except ValueError:
+        try:
+            float(celsius)
+            break
+        except ValueError:
+            print("Nieprawidłowe dane, podaj temperaturę jako wartość liczbową.")
+print('''Wzór na przeliczanie stopni Celsjusza na stopnie Fahrenheita:
+[\u00b0F] = [\u00b0C] * 9/5 + 32''')
+print("Podana temperatura przeliczona na stopnie Fahnrenheita: ", end = "")
+cels_to_fahr(float(celsius))
