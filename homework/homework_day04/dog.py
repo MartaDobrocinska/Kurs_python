ad8be943a6c754d725f2ad1162ed67cb08871408
MartaDobrocinska <m.dobrocinska@gmail.com>
2020-03-17 21:31:03 +0100

    initial commit

---
+++ homework/homework_day04/dog.py
@@ -0,0 +1,11 @@
+while True:
+    age = input("Podaj wiek psa w latach: ")
+    try:
+        float(age)
+        break
+    except ValueError:
+        print("Nieprawidłowe dane, podaj wiek psa jako liczbę.")
+if float(age) <= 2:
+    print("Wiek psa w 'ludzkich' latach: ", (float(age) * 10.5))
+else:
+    print("Wiek psa w 'ludzkich' latach: ", (21 + 4 * (float(age) - 2)))
