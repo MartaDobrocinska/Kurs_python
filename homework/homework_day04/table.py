fcd42ffd3b061eedb91436b4f313213e454bd4e1
MartaDobrocinska <m.dobrocinska@gmail.com>
2020-03-18 23:33:08 +0100

    initial commit

---
+++ homework/homework_day04/table.py
@@ -0,0 +1,24 @@
+def table(data):
+    """Prints a given list in a table format. Sort of"""
+    longest = len(max(data, key=len))
+    top_bottom = ("+" + "-" + ("-" * longest)) * len(data) + "+"
+    top_bottom_30 = ("+" + ("-" * 35)) * len(data) + "+"
+    if longest <= 30:
+        print(top_bottom)
+        for i in data:
+            spaces = longest - len(i)
+            print("|" + i + (spaces * " ") + " ", end="")
+        print("|\n" + top_bottom)
+    else:
+        print(top_bottom_30)
+        for i in data:
+            spaces_30 = 34 - len(i)
+            if len(i) > 30:
+                print("| " + i[0:31] + "...", end="")
+            else:
+                print("|" + i + (spaces_30 * " ") + " ", end="")
+        print("|\n" + top_bottom_30)
+
+
+cells = ["86784", "34534567654", "234567"]
+table(cells)
